3ba506dc73031f16812fece56bbbd25039f32cf2
tusharishere <tusharsinha1452@gmail.com>
2024-02-17 14:57:50 +0530

    trainig pipeline

--- src/CreditCardDefaultPrediction/pipelines/training_pipeline.py
+++ src/CreditCardDefaultPrediction/pipelines/training_pipeline.py
@@ -1,7 +1,7 @@
 from src.CreditCardDefaultPrediction.components.data_ingestion import DataIngestion
 from src.CreditCardDefaultPrediction.components.data_transformation import DataTransformation
 from src.CreditCardDefaultPrediction.components.model_trainer import ModelTrainer
-#from src.CreditCardDefaultPrediction.components.model_evaluation import ModelEvaluation
+from src.CreditCardDefaultPrediction.components.model_evaluation import ModelEvaluation
 
 import os
 import sys
@@ -20,5 +20,5 @@ train_arr, test_arr, val_arr = data_transformation.initiate_data_transformation(
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr,test_arr)
 
-#model_eval_obj = ModelEvaluation()
-#model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(test_arr)
